6da5ae597ffcdb199d67458ab8aa3af616307e15
Nini <lzn_nini@hotmail.com>
2018-04-09 21:02:20 -0700

    230E PS1 Update
    230E PS1 Update

--- 230E/PS1.py
+++ 230E/PS1.py
@@ -3,8 +3,11 @@ import numpy as np
 import math
 from scipy import stats
 import matplotlib.pyplot as plt
+import random
 
 def main():
+	random.seed(12345)
+
 	f = open('PS1.txt','w')
 	df = pd.read_csv("Portfolios_Formed_on_ME_daily.csv")
 	index = df.index[df["Year"]==2010][0]	#only use data till end of 2009
@@ -28,8 +31,9 @@ def main():
 	f.write ("Portfolio daily mean post-1945 is: \n %s\n" % (mean_df(port_df.iloc[pre:,:])))
 
 	# part 1e
-	t, t_ppf = t_test(port_df.iloc[:pre,:], port_df.iloc[pre:,:], 0.05)
+	t, t_ppf = t_test_compare(port_df.iloc[:pre,:], port_df.iloc[pre:,:], 0.05)
 	for i in range(port_n):
+		f.write ("t: %s, t*: %s\n" % (t[i],t_ppf))
 		if t[i] > t_ppf:
 			f.write ("The pre and post 1945 means of portfolio %s are significantly different at 5%% confidence interval\n" 
 				% (col_names[i]))
@@ -58,6 +62,17 @@ def main():
 			port_df.iloc[1:n,-1].reset_index(drop=True))
 		f.write ("The OLS regression of portfolio %s lagged return is: r_VW = %s + %sr_i + %s\n"
 			% (col_names[i], phi, c, np.mean(epsilon)))
+		# part 3b
+		std_err = std_df(epsilon)
+		var_cov = var_cov_mat(port_df.iloc[0:n-1,i].reset_index(drop=True), std_err)	#var(beta)=sigma^2(X'X)^-1[1,1]
+		s_phi = np.sqrt(var_cov[1,1])	#n-k-1 dof
+		t, t_ppf = t_test(phi, 0, s_phi, 0, n, 0, 0.05)
+		if t > t_ppf:
+			f.write ("t: %s, t*: %s. The slope phi of portfolio %s is significant at 5%% confidence interval\n" 
+				% (t, t_ppf, col_names[i]))
+		else:
+			f.write ("t: %s, t*: %s. The slope phi of portfolio %s is NOT significant at 5%% confidence interval\n" 
+				% (t, t_ppf, col_names[i]))
 
 	# part 4
 	Y_0 = 0
@@ -83,12 +98,21 @@ def main():
 	f.close()
 
 def mean_df(df):
+	"""
+	Calculate mean of dataframe by column
+	"""
 	return df.mean()
 
 def std_df(df):
+	"""
+	Calculate standard deviation of a dataframe by column
+	"""
 	return df.std()
 
-def t_test(df1, df2, CI):
+def t_test_compare(df1, df2, CI):
+	"""
+	t-test to compare two means
+	"""
 	mu1 = df1.mean()
 	mu2 = df2.mean()
 	std1 = df1.std()
@@ -100,7 +124,31 @@ def t_test(df1, df2, CI):
 	t_ppf = stats.t.ppf(1-CI/2, n1+n2-2)	#two-tail
 	return t, t_ppf
 
+def t_test(mu1, mu2, std1, std2, n1, n2, CI):
+	"""
+	Returns general t-test to check confidence interval
+	"""
+	s_p = np.sqrt(((n1-1)*pow(std1,2)+(n2-1)*pow(std2,2))/(n1+n2-2))
+	if n2 == 0:
+		t = np.abs(mu1-mu2)/(s_p*np.sqrt(1/n1))
+	else:
+		t = np.abs(mu1-mu2)/(s_p*np.sqrt(1/n1+1/n2))
+	t_ppf = stats.t.ppf(1-CI/2, n1+n2-2)	#two-tail
+	return t, t_ppf
+
+def var_cov_mat(x, sigma):
+	"""
+	Returns variance-covariance matrix from linear regression model
+	"""
+	nrow = len(x)
+	one = pd.Series([1 for i in range(nrow)])
+	new_df = pd.concat([one,x], axis=1)
+	return np.dot(new_df.T,new_df) * np.power(sigma,2)
+
 def find_par(x, y):
+	"""
+	Returns linear regression model parameters
+	"""
 	nrow = len(x)
 	one = pd.Series([1 for i in range(nrow)])
 	new_df = pd.concat([one,x,y], axis=1)
@@ -113,6 +161,9 @@ def find_par(x, y):
 	return weight[0,0], weight[1,0], np.squeeze(np.asarray(err))
 
 def durbin_watson(e_t, e_prev):
+	"""
+	Returns durbin watson result to test autocorrelation
+	"""
 	dw = sum(pow(e_t-e_prev[1:],2)) / sum(pow(e_t,2))
 	result = ""
 	if dw == 2:
@@ -126,6 +177,9 @@ def durbin_watson(e_t, e_prev):
 	return result
 
 def ar1_generator(n, Y_0, phi, p):
+	"""
+	Returns a string of AR(1) simulation numbers
+	"""
 	Y_t = []
 	samples = np.random.normal(0,1,n)
 	for i in range(n):
